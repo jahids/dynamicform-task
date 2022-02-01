c0162b4ddd02bc922a63ade43e1c2d953234c5f3
jahids <evanjahid321@gmail.com>
2022-02-01 20:45:04 +0600

    "pass validate & url validate done"

--- init.js
+++ init.js
@@ -1,10 +1,11 @@
 import FormValidation from "./validation.js"
 
 new FormValidation(document.getElementById('form'), {
-    'first-name': { minLength: 2 },
-    'last-name': { minLength: 3 },
+    'first-name': { minLength: 2, nonNumeric: true,  maxLength: 5 },
+    'last-name': { minLength: 3, nonNumeric: true },
     'my-email': { minLength: 5 },
     'phone': { minLength: 11, maxLength: 12 },
+    'password': { hasUpperCase: true,  hasLowerCase: true },
+     'url': { hasHttps: true }
     
 })
-
--- validation.js
+++ validation.js
@@ -6,106 +6,82 @@ export default class FormValidation {
         if (!form || !options) {
             console.error("Invalid parameter");
         }
-
         this.formElement = form;
         this.options = options;
-
-
-
         this.formElement.addEventListener("submit", (e) => this.validateForm(e));
     }
-
-
-
     validateForm(e) {
-
         e.preventDefault();
-
         const inputNames = Object.keys(this.options)
-        // console.log(inputNames);
+
         inputNames.forEach(name => {
             const input = this.formElement.querySelector(`[name="${name}"]`);
-            // console.log(name);
 
-            // console.log({ name });
-            // console.log(this.options[name]);
-
-            //    name chek
             // input validate
             if ('minLength' in this.options[name]) {
                 const minLength = this.options[name].minLength;
-
-                if (input.value.length > minLength) {
-
-                    document.getElementById(name).innerText = `${name} is valid`;
-                    // console.log(name, "this is valid");
-
-
+                const nonNumeric = this.options[name].nonNumeric;
+                if (input.value.length > minLength && (/[0-9]+/).test(input.value) !== nonNumeric) {
+                    setsuccess('valid')
                 } else {
-                    // console.log('-----', name, minLength);
-                    // console.log(name, 'this is not vlaid');
-                    document.getElementById(name).innerText = `${name} is invalid`;
-
-
-
-
+                    seteror("invalid");
                 }
-
-
-
-
-
-
-                // console.log(this.options[name].maxLength);
-
-                //  maxlength
-
-                if ('maxLength' in this.options[name]) {
-
-                    const maxLength = this.options[name].maxLength;
-
-                    if (input.value.length < maxLength) {
-
-                        document.getElementById(name).innerText = `${name} is valid`;
-                        console.log(name, "this is valid");
-
-
-                    } else {
-                        // console.log('-----', name, minLength);
-                        console.log(name, 'this is not vlaid');
-                        document.getElementById(name).innerText = `${name} is invalid`;
-
-                    }
-
-
-
-
-
-                }
-
-
-
-
-
-
-
             }
 
+            //  maxlength
+            if ('maxLength' in this.options[name]) {
+                const maxLength = this.options[name].maxLength;
+                if (input.value.length > null && input.value.length < maxLength) {
 
+                    setsuccess('valid')
+                } else {
+                    seteror("invalid");
+                }
+            }
+            // lowercase
+            if ('hasLowerCase' in this.options[name]) {
+                const hasLowerCase = this.options[name].hasUpperCase;
+                if ((/[a-z]/gm).test(input.value) == hasLowerCase) {
+                    setsuccess('valid')
+                } else {
+                    seteror("invalid");
+                }
+            }
 
+            //  url,
+            if ('hasHttps' in this.options[name]) {
 
+                const hasHttps = this.options[name].hasHttps;
+                console.log(hasHttps);
 
+                if ((/https/g).test(input.value) == hasHttps) {
+                    setsuccess('valid')
+                } else {
+                    seteror("invalid");
+                }
+            }
 
+            // HASSUPERCASE
+            if ('hasUpperCase' in this.options[name]) {
+                const hasUpperCasenew = this.options[name].hasUpperCase;
+                console.log(input.value);
+                if ((/[A-Z]/gm).test(input.value) == hasUpperCasenew) {
 
+                    setsuccess('valid')
+                } else {
+                    seteror("invalid");
 
+                }
+            }
+            // eror function
+            function seteror(x) {
 
+                return document.getElementById(name).innerText = `${name}  ${x}`;
+            }
+            // eror succes funtion
+            function setsuccess(x) {
+                return document.getElementById(name).innerText = `${name}  ${x}`;
+            }
         })
-
-
-
     }
-
-
-
-
 }
